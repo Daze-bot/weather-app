4b80b96518c30a3ab50efa10bf279f65c905fbf9
Code4Daze <jevon.fillette@gmail.com>
2022-09-14 19:06:46 -0400

    Show Current Temp on screen

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import './css/style.css';
 import Weather from './scripts/api';
 
 let currentWeather = new Weather('Philadelphia', 'f');
+let p = document.createElement('p');
+document.body.appendChild(p);
 
-currentWeather.getWeatherData();
+// Option 3
+async function main() {
+  let result = await currentWeather.getCurrentTemp();
+  p.textContent = result;
+}
+
+main();
--- src/scripts/api.js
+++ src/scripts/api.js
@@ -6,19 +6,21 @@ class Weather {
     } else if (units === 'f') {
       this.units = 'imperial';
     }
-    this.temp = undefined;
   }
 
   async getWeatherData() {
     try {
       let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.location}&units=${this.units}&APPID=59e393b0ab45a1585d147ab33d507f28`);
       let weatherData = await response.json();
-      console.log(weatherData);
-      this.temp = weatherData.main.temp;
-      console.log(this.temp);
+      return weatherData;
     } catch (error) {
       console.error(error);
     }
+  }
+
+  async getCurrentTemp() {
+    let fullData = await this.getWeatherData();
+    return fullData.main.temp;
   }
 }
 
